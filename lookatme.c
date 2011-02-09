b1a06a0f45efd74f5738766a06b3a01fc1deccdf
Salvador <sfandino@yahoo.com>
2011-02-09 18:46:24 +0100

    inspect the code generated by GCC

---
+++ lookatme.c
@@ -0,0 +1,28 @@
+
+typedef __int128 int128_t;
+
+int128_t
+add128(int128_t a, int128_t b) {
+  return a + b;
+}
+
+int128_t
+sub128(int128_t a, int128_t b) {
+  return a - b;
+}
+
+int128_t
+mul128(int128_t a, int128_t b) {
+  return a * b;
+}
+
+int128_t
+div128(int128_t a, int128_t b) {
+  return a / b;
+}
+
+int128_t
+rem128(int128_t a, int128_t b) {
+  return a % b;
+}
+
