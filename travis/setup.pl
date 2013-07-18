390caf1589351421ba5f043e9797606317793be4
Salvador <sfandino@yahoo.com>
2013-07-18 09:51:24 +0200

    fix syntax error

--- travis/setup.pl
+++ travis/setup.pl
@@ -4,7 +4,7 @@ use strict;
 use warnings;
 
 unless ($ENV{TRAVIS}) {
-    die "This script is only intended to be run from Travis CI platform";
+    die "This script is only intended to be run from Travis CI platform\n";
 }
 
 unlink "perl_math_int64.h";
@@ -21,5 +21,5 @@ chdir "dependencies" or die "unable to chdir to dependencies: $!";
 system "git clone https://github.com/salva/p5-Module-CAPIMaker.git";
 system "cd p5-Module-CAPIMaker && perl Makefile.PL && make install";
 
-system "git clone https://github.com/salva/p5-Math-Int64.git"
+system "git clone https://github.com/salva/p5-Math-Int64.git";
 system "cd p5-Math-Int64 && perl Makefile.PL && make install";
