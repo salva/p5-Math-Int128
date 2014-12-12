0a76af3b3bc4f6b763ff98a3a42008e884a2d5a0
Dave Rolsky <autarch@urth.org>
2014-12-11 20:45:38 -0600

    Update CAPIMaker plugin

--- inc/CreateCAPI.pm
+++ inc/CreateCAPI.pm
@@ -2,6 +2,9 @@ package inc::CreateCAPI;
 
 use strict;
 use warnings;
+use autodie;
+
+use Dist::Zilla::File::InMemory;
 
 use Moose;
 
@@ -21,6 +24,34 @@ sub gather_files {
     $self->log( ["Running @cmd"] );
 
     system(@cmd) and die "Could not run @cmd";
+
+    $self->_add_and_clean_file($_) for qw(
+        c_api.h
+        c_api_client/perl_math_int128.c
+        c_api_client/perl_math_int128.h
+        c_api_client/sample.xs
+    );
+
+    return;
+}
+
+sub _add_and_clean_file {
+    my $self = shift;
+    my $file = shift;
+
+    open my $fh, '<', $file;
+    my $content = do { local $/; <$fh> };
+    close $fh;
+
+    $self->add_file(
+        Dist::Zilla::File::InMemory->new(
+            name     => $file,
+            content  => $content,
+            added_by => [__PACKAGE__],
+        )
+    );
+
+    unlink $file;
 
     return;
 }
