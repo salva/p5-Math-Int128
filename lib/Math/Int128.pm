a1445d08c6e0be48df0aacbf6f41dbe53083cbfc
Dave Rolsky <autarch@urth.org>
2015-01-05 09:05:41 -0600

    Set version to 0.18

--- lib/Math/Int128.pm
+++ lib/Math/Int128.pm
@@ -6,7 +6,7 @@ use warnings;
 # The "our $VERSION" bit needs to be at the beginning of the line for the
 # benefit of the RewriteVersion plugin.
 BEGIN {
-our $VERSION = '0.17_03';
+our $VERSION = '0.18';
 require XSLoader;
 XSLoader::load('Math::Int128', $VERSION);
 }
